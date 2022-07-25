7acbac3bf98d47de56e57cad32f4b9c3352041ae
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2022-07-25 17:59:47 +0200

    RTL direction

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,28 +1,64 @@
+//import { useRouter } from 'next/router'
+
 import { PrismicLink } from "@prismicio/react";
 import { linkResolver } from "../prismicio";
 
 
 const LangIcon = ({ lang }) => {
-    const code = lang.substring(3).toLowerCase();
-    return <span className={`fi fi-${code}`} />;
+  const code = lang.substring(3).toLowerCase();
+  return <span className={`fi fi-${code}`} />;
 };
 
 
 export const LanguageSwitcher = ({ altLangs = [] }) => {
-    return (
-        <ul className="-ml-4 -mt-4 flex flex-wrap">
-        {altLangs.map((altLang) => {
-          return (
-            <li key={altLang.lang}  className="pl-4 pt-4">
-              <PrismicLink href={linkResolver(altLang)} locale={altLang.lang}>
-                <span className="sr-only">{altLang.lang}</span>
-                <LangIcon lang={altLang.lang} />
-              </PrismicLink>
-            </li>
-          );
-        })}
-      </ul>
-    );
+
+  // const router = useRouter()
+
+  // const onSelectChange = (e) => {
+  //   const locale = e.target.value;
+  //   router.push(router.asPath, router.asPath, {
+  //     locale,
+  //     scroll: false
+  //   })
+  // }
+
+  return (
+    <ul className="-ml-4 -mt-4 flex flex-wrap">
+      {altLangs.map((altLang) => {
+        return (
+          <li key={altLang.lang} className="pl-4 pt-4">
+            <PrismicLink href={linkResolver(altLang)} locale={altLang.lang}>
+              <span className="sr-only">{altLang.lang}</span>
+              <LangIcon lang={altLang.lang} />
+            </PrismicLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
+
+// <li>
+//   <select name="languages" onChange={onSelectChange}>
+//     {altLangs.map((altLang) => (
+//       <option key={altLang.lang} value={altLang.lang}>
+//         {altLang.lang}
+//       </option>
+//     ))}
+//   </select>
+// </li>
+
+//   <ul className="-ml-4 -mt-4 flex flex-wrap">
+//   {altLangs.map((altLang) => {
+//     return (
+//       <li key={altLang.lang}  className="pl-4 pt-4">
+//         <PrismicLink href={linkResolver(altLang)} locale={altLang.lang}>
+//           <span className="sr-only">{altLang.lang}</span>
+//           <LangIcon lang={altLang.lang} />
+//         </PrismicLink>
+//       </li>
+//     );
+//   })}
+// </ul>
 
 export default LanguageSwitcher;
