06936e98164e0f4b6a03a13faad1e3ca42229b02
Ahmed.mohsen <ahmed.mohsen@akqa.com>
2022-07-26 18:01:57 +0200

    Add the form to the index file

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,37 @@ import { createClient } from "../prismicio";
 import { components } from "../slices/";
 import { Layout } from "../components/Layout";
 
-const Index = ({ doc, navigation, settings }) => {
+import React from 'react';
+import { formium } from '../lib/formium';
+import { FormiumForm } from '@formium/react';
+
+const Index = ({ doc, navigation, settings, form }) => {
+
+  const [success, setSuccess] = React.useState(false);
+  if (success) {
+    return <div>Thank you! Your response has been recorded.</div>;
+  }
+
   return (
     <Layout
       altLangs={doc.alternate_languages}
       navigation={navigation}
       settings={settings}
     >
+      
       <Head>
         <title>{prismicH.asText(doc.data.title)}</title>
       </Head>
+      
       <SliceZone slices={doc.data.slices} components={components} />
+      <div className="flex justify-center pt-36 pb-36">
+          <FormiumForm data={form}
+            onSubmit={async (values) => {
+              await formium.submitForm('registration-form', values);
+              setSuccess(true);
+            }}
+          />
+      </div>
     </Layout>
   );
 };
@@ -29,12 +49,14 @@ export async function getStaticProps({ locale, previewData }) {
   const page = await client.getByUID("page", "home", { lang: locale });
   const navigation = await client.getSingle("navigation", { lang: locale });
   const settings = await client.getSingle("settings", { lang: locale });
+  const form = await formium.getFormBySlug('registration-form');
 
   return {
     props: {
       doc: page,
       navigation,
       settings,
+      form,
     },
   };
 }
